cc14e7cfbd97c60d049aa1aca0d1e63f6d6513e9
FayyadKhalil <224110147@psu.edu.sa>
2026-03-14 17:18:16 +0300

    Update VehicleRental.java

--- src/main/java/com/mycompany/vehiclerentalmain/VehicleRental.java
+++ src/main/java/com/mycompany/vehiclerentalmain/VehicleRental.java
@@ -285,58 +285,81 @@ public class VehicleRentalSystem
     }
     public void viewAllAvailableCars()
     {
+        boolean found = false;
         for(int i = 0; i < vehicles.size(); i++)
         {
             if(vehicles.get(i).getAvailabilityStatus().equalsIgnoreCase("Available"))
                 System.out.println(vehicles.get(i));
+            found = true;
         }
+        if(!found)
+            System.out.println("There are no available cars");
     }
     
     public void viewRentedCars()
     {
+        boolean found = false;
         for(int i = 0; i < vehicles.size(); i++)
         {
             if(vehicles.get(i).getAvailabilityStatus().equalsIgnoreCase("Rented"))
                 System.out.println(vehicles.get(i));
+            found = true;
         }
+        if(!found)
+            System.out.println("There are no rented cars");
     }
     
     public void filterByPrice(double price)
     {
+        boolean found = false;
         for(int i = 0; i< vehicles.size(); i++)
         {
             if(vehicles.get(i).getRentalPricePerDay() <= price)
                 System.out.println(vehicles.get(i));
+            found = true;
         }
+        if(!found)
+            System.out.println("There are no cars by this price");
     }
     
     public void filterbyBrand(String brand)
     {
+        boolean found = false;
         for(int i = 0; i< vehicles.size(); i++)
         {
             if(vehicles.get(i).getBrand().equalsIgnoreCase(brand))
                 System.out.println(vehicles.get(i));
+            found = true;
         }
+        if(!found)
+            System.out.println("There are no cars by this brand");
     }
     
     public void filterbyYear(int year)
     {
+        boolean found = false;
         for(int i = 0; i < vehicles.size(); i++)
         {
             if(vehicles.get(i).getYear() == year)
                 System.out.println(vehicles.get(i));
+            found = true;
         }
+        if(!found)
+            System.out.println("There are no cars by this year");
     }
     
     public void fitlerBycolor(String color)
     {
+        boolean found = false;
         for(int i = 0; i < vehicles.size(); i++)
         {
             if(vehicles.get(i).getColor().equalsIgnoreCase(color))
                 System.out.println(vehicles.get(i));
+            found = true;
         }
+        if(!found)
+            System.out.println("There are no cars by this color");
     }
 }
 
 // Fayadh will continue R3 when R6 is Done <----- (important)
-
