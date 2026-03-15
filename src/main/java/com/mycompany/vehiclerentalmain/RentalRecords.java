dcc379f51d317d6d9612dc006181b91a35071c28
FayyadKhalil <224110147@psu.edu.sa>
2026-03-16 00:17:34 +0300

    Create RentalRecords.java

---
+++ src/main/java/com/mycompany/vehiclerentalmain/RentalRecords.java
@@ -0,0 +1,74 @@
+
+package com.mycompany.vehiclerentalmain;
+
+public class RentalRecords 
+{
+    private String recordID;
+    private final VehicleRental vehicle;
+    private String customerName;
+    private int rentalDays;
+    private final double totalCost;
+    private boolean active;
+
+    public RentalRecords(String recordID, VehicleRental vehicle, String customerName, int rentalDays, double totalCost, boolean active) 
+    {
+        if(recordID == null)
+            throw new IllegalArgumentException("The recordID cannot be null.");
+        else this.recordID = recordID;
+        
+        this.vehicle = vehicle;
+        
+        if(customerName == null || customerName.isBlank())
+            throw new IllegalArgumentException("The customer name cannot be empty.");
+        else this.customerName = customerName;
+        
+        if(rentalDays <= 0)
+            throw new IllegalArgumentException("The rental days must be more than zero.");
+        else this.rentalDays = rentalDays;
+        
+        this.totalCost = totalCost;
+        this.active = active;
+    }
+
+    public String getRecordID() {
+        return recordID;
+    }
+
+    public VehicleRental getVehicle() {
+        return vehicle;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public int getRentalDays() {
+        return rentalDays;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+    
+    @Override
+    public String toString() 
+    {
+        return "Rental Record\n" +
+           "-----------------------\n" +
+           "Record ID: " + recordID + "\n" +
+           "Vehicle ID: " + vehicle.getVehicleid() + "\n" +
+           "Customer Name: " + customerName + "\n" +
+           "Rental Days: " + rentalDays + "\n" +
+           "Total Cost: " + totalCost + "\n" +
+           "Active?: " + active + "\n";
+    }
+    
+}
